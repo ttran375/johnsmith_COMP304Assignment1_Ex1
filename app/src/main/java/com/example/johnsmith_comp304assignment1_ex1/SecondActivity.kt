49eeced37c1620a442d1fabe1d7b177dbc8f9672
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-03 11:50:34 +0000

    Implement SecondActivity class

--- app/src/main/java/com/example/johnsmith_comp304assignment1_ex1/SecondActivity.kt
+++ app/src/main/java/com/example/johnsmith_comp304assignment1_ex1/SecondActivity.kt
@@ -1,20 +1,25 @@
 package com.example.johnsmith_comp304assignment1_ex1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class SecondActivity : AppCompatActivity() {
+
+
+    private lateinit var textViewName: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        textViewName = findViewById(R.id.textViewName)
+
+
+        val name = intent.getStringExtra("name")
+        textViewName.text = name
     }
 }
