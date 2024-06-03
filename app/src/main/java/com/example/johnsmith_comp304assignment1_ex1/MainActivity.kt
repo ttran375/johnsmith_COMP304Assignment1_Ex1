dc7b86afc8400efacd6c81a3da206f74d9c6046a
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-03 07:46:00 -0400

    Implement MainActivity class

--- app/src/main/java/com/example/johnsmith_comp304assignment1_ex1/MainActivity.kt
+++ app/src/main/java/com/example/johnsmith_comp304assignment1_ex1/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.johnsmith_comp304assignment1_ex1
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
+    private lateinit var editTextName: EditText
+    private lateinit var buttonSend: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        editTextName = findViewById(R.id.editTextName)
+        buttonSend = findViewById(R.id.buttonSend)
+
+
+        buttonSend.setOnClickListener {
+            val name = editTextName.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("name", name)
+            startActivity(intent)
         }
     }
 }
